e019de60ceb54f2a5280f2f187a745ad4226c625
Florent NOEL <florent.noel@protonmail.com>
2020-06-16 22:56:28 +0200

    Add eslint/prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const App = ({ title }) =>
-  <div>{title}</div>;
+const App = ({ title }) => <div>{title}</div>;
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default App;
