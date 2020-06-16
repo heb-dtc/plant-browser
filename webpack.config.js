3af95b83656ec09512220f080d8ffb4c1685f525
Florent NOEL <florent.noel@protonmail.com>
2020-06-16 23:02:39 +0200

    Webpack config for dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,9 @@
-const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  output: {
-    path: `${__dirname}/dist`,
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-  },
+const commonConfig = require('./webpack.common.js');
+
+module.exports = ({ env, addon }) => {
+  const envConfig = require(`./webpack.${env}.js`);
+
+  return webpackMerge(commonConfig, envConfig);
 };
